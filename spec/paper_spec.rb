b3292528cb18e6b43f8d8c4e906e87e499750097
Patrick <veith.16@osu.edu>
2017-10-09 13:06:15 -0400

    Refactored paper tests to create paper object before each test

--- spec/paper_spec.rb
+++ spec/paper_spec.rb
@@ -1,25 +1,26 @@
 require 'spec_helper'
 
 describe Paper do
+  before :each do
+    @paper = Paper.new
+  end
+
   describe "#initialize" do
     context "when a new class is initialized" do
       it "should not be nil" do
-        paper = Paper.new
-        expect(paper).to be_truthy
+        expect(@paper).to be_truthy
       end
 
       it "should be a paper object" do
-        paper = Paper.new
-				expect(paper).to be_instance_of(Paper)
+				expect(@paper).to be_instance_of(Paper)
 			end
     end
 
     context "when a paper is written on by a pencil" do
       it "should contain the written text" do
-        paper = Paper.new
-        pencil = Pencil.new(paper)
+        pencil = Pencil.new(@paper)
         pencil.write("hello")
-        expect(paper.text).to eq("hello")
+        expect(@paper.text).to eq("hello")
       end
     end
   end
