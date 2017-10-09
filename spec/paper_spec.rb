7d40d3c1e7e42731f72c35c806d17ac9cb7c19c0
Patrick <veith.16@osu.edu>
2017-10-09 13:10:47 -0400

    Removed redundant test

--- spec/paper_spec.rb
+++ spec/paper_spec.rb
@@ -15,13 +15,5 @@ describe Paper do
 				expect(@paper).to be_instance_of(Paper)
 			end
     end
-
-    context "when a paper is written on by a pencil" do
-      it "should contain the written text" do
-        pencil = Pencil.new(@paper)
-        pencil.write("hello")
-        expect(@paper.text).to eq("hello")
-      end
-    end
   end
 end
