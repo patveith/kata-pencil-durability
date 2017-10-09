8fa4300723811bfa2ad8f40fc7da1aa28eb840b6
Patrick <veith.16@osu.edu>
2017-10-09 00:19:46 -0400

    Create pencil objects before each test case

--- run.rb
+++
@@ -1,7 +0,0 @@
-require "./lib/pencil"
-
-##
-# Method to write with the pencil
-begin
-	pencil = Pencil.new
-end
--- spec/pencil_spec.rb
+++ spec/pencil_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
 describe Pencil do
+	before :each do
+		@pencil = Pencil.new
+	end
 
 	describe "#initialize" do
 		context "when a new class is initialized" do
 			it "should not be nil" do
-				pencil = Pencil.new
-				expect(pencil).to be_truthy
+				expect(@pencil).to be_truthy
 			end
 
 			it "should be a pencil object" do
-				pencil = Pencil.new
-				expect(pencil).to be_instance_of(Pencil)
+				expect(@pencil).to be_instance_of(Pencil)
 			end
 		end
 	end
@@ -19,9 +20,7 @@ describe Pencil do
 	describe "#write" do
 		context "when a pencil is given a string" do
 			it "should write the string" do
-				pencil = Pencil.new
-
-				expect {pencil.write("hello")}.to output("hello").to_stdout
+				expect {@pencil.write("hello")}.to output("hello").to_stdout
 			end
 		end
 	end
